9b7c72d159a28f823753ea4d17b496ba654c4bd8
hoeskiee <hoeskiee@gmail.com>
2024-05-21 01:08:18 +0700

    add controller to create new user

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -8,9 +8,9 @@ async function getUserById(req, res){
         const user = await userModel.getUserById(userId);
 
         res.json({
-            success : true,
+            success : true, 
             user : user
-        });
+    }); 
     } catch(e) {
         console.error('Failed to retrieve user data : ', e);
         res.status(500).json({
@@ -20,6 +20,27 @@ async function getUserById(req, res){
     }
 }
 
+// * Controller to receive req from frontend to add new user
+async function createUser(req, res){
+    const { userId, photoUrl, userRole, userName, phoneNumber, email, isVerified } = req.body;
+
+    try {
+        const newUser = await userModel.createUser(userId, photoUrl, userRole, userName, phoneNumber, email, isVerified);
+
+        res.json({
+            success : true,
+            message : "User has been created!",
+        });
+    } catch(e) {
+        console.error("Failed to create new user : ", e);
+        res.status(500).json({
+            success : false,
+            message : `Failed to create new user : ${e}`,
+        });
+    }
+}
+
 module.exports = {
     getUserById,
+    createUser,
 };
