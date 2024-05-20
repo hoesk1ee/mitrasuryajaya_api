b80f88b2fda6deafa698149b28fd82d96287215b
hoeskiee <hoeskiee@gmail.com>
2024-05-20 22:15:57 +0700

    Change postgres user

--- db/index.js
+++ db/index.js
@@ -1,21 +1,21 @@
 const { Pool } = require('pg');
 
 // * POstgres user for Hoeskie
-// const pool = new Pool({
-//     user: 'hoeskie',
-//     host: 'localhost',
-//     database: 'mitra_surya_jaya',
-//     password: 'postgres',
-//     port: 5432,
-// });
-
-// * Postgres user for JeSan05
 const pool = new Pool({
-    user: 'postgres',
+    user: 'hoeskie',
     host: 'localhost',
     database: 'mitra_surya_jaya',
     password: 'postgres',
     port: 5432,
 });
+
+// * Postgres user for JeSan05
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'mitra_surya_jaya',
+//     password: 'postgres',
+//     port: 5432,
+// });
 
 module.exports = pool;
