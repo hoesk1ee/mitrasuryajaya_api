453f93a18df357e62f2bc923415a03d9c17ac65c
hoeskiee <hoeskiee@gmail.com>
2024-05-25 23:06:50 +0700

    Add locale date time for exp_date data

--- controller/product_exp_controller.js
+++ controller/product_exp_controller.js
@@ -1,4 +1,6 @@
 const productExpModel = require('../model/product_exp_model');
+const moment = require('moment');
+require('moment/locale/id');
 
 // * Controller to get all product exp based on product_detail_id
 async function getAllProductExp(req,res){
@@ -18,12 +20,16 @@ async function getAllProductExp(req,res){
                 product_detail_pic : product_exp[0].product_detail_pic,
                 product_detail_name : product_exp[0].product_detail_name,
                 price : product_exp[0].price,
-                product_expired : product_exp.map(({exp_date, quantity, product_barcode}) => ({exp_date, quantity, product_barcode}))
+                product_data : product_exp.map(({exp_date, quantity, product_barcode}) => ({
+                    exp_date : moment(exp_date).locale('id').format('DD MMMM YYYY'), 
+                    quantity, 
+                    product_barcode
+                }))
             });
         }
     } catch(e){
         console.error('Error while fetching product expired : ', e);
-        res.status(500).json({ success : false, message : "Internal Server Error ", e});
+        res.status(500).json({ success : false, message : `Internal Server Error : ${e}`});
     }
 };
 
