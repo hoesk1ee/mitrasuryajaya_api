29edf71320ba1592a194c68b7976030181d980fa
hoeskiee <hoeskiee@gmail.com>
2024-05-25 12:15:24 +0700

    delete unused pool user

--- db/index.js
+++ db/index.js
@@ -1,15 +1,5 @@
 const { Pool } = require('pg');
 
-// * POstgres user for Hoeskie
-// const pool = new Pool({
-//     user: 'hoeskie',
-//     host: 'localhost',
-//     database: 'mitra_surya_jaya',
-//     password: 'postgres',
-//     port: 5432,
-// });
-
-// * Postgres user for JeSan05
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
