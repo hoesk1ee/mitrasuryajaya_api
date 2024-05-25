028a2ff4f8785349c7fef8ad4dc01ddd274bd34e
JeSan05 <jessicasanchia05@gmail.com>
2024-05-25 15:57:21 +0700

    Make product exp model

---
+++ model/product_exp_model.js
@@ -0,0 +1,20 @@
+const pool = require('../db/index');
+
+// * Read all product exp
+async function getAllProductExp(){
+    const query = `
+        SELECT pd.product_detail_name, pe.product_detail_id, pe.exp_date, pe.quantity, pe.product_barcode 
+        FROM product_exp pe 
+        JOIN product_detail pd ON pe.product_detail_id = pd.product_detail_id
+        WHERE pe.product_detail_id = $1;`;
+
+    const values = [productDetailId];
+
+    const result = await pool.query(query, values);
+
+    return result.rows;
+};
+
+module.exports = {
+    getAllProductExp
+}
