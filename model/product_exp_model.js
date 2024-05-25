4617f00011beca210feeeabb7477f40304b58be5
JeSan05 <jessicasanchia05@gmail.com>
2024-05-25 22:49:47 +0700

    fex getAllProductExp and npm install moment

--- model/product_exp_model.js
+++ model/product_exp_model.js
@@ -1,12 +1,12 @@
 const pool = require('../db/index');
 
 // * Read all product exp
-async function getAllProductExp(){
+async function getAllProductExp(productDetailId){
     const query = `
         SELECT pd.product_detail_pic, pd.product_detail_name, pd.price, pe.product_detail_id, pe.exp_date, pe.quantity, pe.product_barcode 
         FROM product_exp pe 
         JOIN product_detail pd ON pe.product_detail_id = pd.product_detail_id
-        WHERE pe.product_detail_id = $1;`;
+        WHERE pe.product_detail_id = $1`;
 
     const values = [productDetailId];
 
--- routes/product_exp.js
+++ routes/product_exp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const productExpController = require('../controller/product_exp_controller');
 const router = express.Router();
 
-// * Router endpoint to get all product expired based on product_detail_id
-app.use('/:productDetailId', productExpController.getAllProductExp);
+// * Router endpoint to fetch all product expired based on product_detail_id
+router.get('/:productDetailId', productExpController.getAllProductExp);
 
 module.exports = router;
