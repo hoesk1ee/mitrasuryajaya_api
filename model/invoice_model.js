ab52edfd2e4ba44da00a1929917675bfc344c7fa
JeSan05 <jessicasanchia05@gmail.com>
2024-05-31 22:55:02 +0700

    fix query in getAllInvoice

--- model/invoice_model.js
+++ model/invoice_model.js
@@ -2,11 +2,22 @@ const pool = require('../db/index');
 
 // * Read All Invoice
 async function getAllInvoice(){
-    const query = `SELECT * FROM invoice`;
+    const query = `
+        SELECT 
+            i.*, 
+            c.customer_name,
+            COUNT(ii.quantity) AS total_item 
+        FROM invoice i 
+        JOIN customers c ON i.customer_id = c.customer_id
+        LEFT JOIN invoice_item ii ON i.invoice_id = ii.invoice_id 
+        GROUP BY i.invoice_id, c.customer_id
+    `;
     const result = await pool.query(query);
     
     return result.rows;
 };
+
+
 
 module.exports = {
     getAllInvoice
